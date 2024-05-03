e0233253105de914ba7f625099b8a9ef3713ee96
Luis <166702635+IannelloDev@users.noreply.github.com>
2024-05-03 15:22:14 +0000

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Definición de las listas de excusas
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Función para generar una excusa aleatoria
+  function generarExcusa() {
+    let excusa =
+      who[Math.floor(Math.random() * who.length)] +
+      " " +
+      action[Math.floor(Math.random() * action.length)] +
+      " " +
+      what[Math.floor(Math.random() * what.length)] +
+      " " +
+      when[Math.floor(Math.random() * when.length)];
+    return excusa;
+  }
+
+  // Modificar el contenido del elemento con ID "excuse"
+  document.getElementById("excuse").innerHTML = generarExcusa();
 };
