c9e81bd708b9b78603e9bce0d68f10a29b411d00
Luis <166702635+LuisIannelloDev@users.noreply.github.com>
2024-06-13 13:05:29 +0000

    Card-generator-Luis-Iannello

--- src/app.js
+++ src/app.js
@@ -5,32 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  // Definición de las listas de excusas
-  let who = ["The dog", "My grandma", "The mailman", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "my phone", "the car"];
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
   ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
 
-  // Función para generar una excusa aleatoria
-  function generarExcusa() {
-    let excusa =
-      who[Math.floor(Math.random() * who.length)] +
-      " " +
-      action[Math.floor(Math.random() * action.length)] +
-      " " +
-      what[Math.floor(Math.random() * what.length)] +
-      " " +
-      when[Math.floor(Math.random() * when.length)];
-    return excusa;
-  }
+let generateRandomSuit = () => {
+  let suit = ["spade", "club", "diamond", "heart"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
 
-  // Modificar el contenido del elemento con ID "excuse"
-  document.getElementById("excuse").innerHTML = generarExcusa();
+  return suit[indexSuit];
 };
